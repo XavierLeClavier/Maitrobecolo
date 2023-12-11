7fe65b91c40fcb50b35479d94049c8f0f4cbfa35
Xavi <xavier.stoa@gmail.com>
2023-12-11 13:03:26 +0100

    mov a mp4

--- pages/easteregg.vue
+++ pages/easteregg.vue
@@ -2,7 +2,7 @@
     <Header></Header>
     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="/assets/img/mehdidonc.png" alt="MEHDI"></a>
     <video width="1920" height="1080" controls>
-        <source src="../assets/video/non.mov" type=video/ogg>
+        <source src="../assets/video/non.mp4" type=video/ogg>
     </video>
     <Footer></Footer>
 </template>
